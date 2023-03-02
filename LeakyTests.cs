ef8c3b9eccd571618d93fc294b4d5b472b33eb77
Jonathan Peppers <jonathan.peppers@microsoft.com>
2023-03-02 17:02:28 -0600

    Update LeakyTests.cs

--- LeakyTests.cs
+++ LeakyTests.cs
@@ -6,15 +6,37 @@ namespace wpf_memory_leaks;
 public class LeakyTests
 {
     [UIFact]
-    public async Task Test1()
+    public async Task BindingSource()
     {
         WeakReference reference;
-        Binding binding = new Binding("Text");
-        binding.Source = new { Text = "Foo" };
+        var data = new { Text = "Foo" };
 
         {
+            var binding = new Binding("Text") { Source = data };
             var text = new TextBlock();
             reference = new WeakReference(text);
+            text.SetBinding(TextBlock.TextProperty, binding);
+            Assert.Equal("Foo", text.Text);
+        }
+        
+        await Task.Yield();
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+
+        Assert.False(reference.IsAlive, "TextBlock should not be alive!");
+    }
+
+    [UIFact]
+    public async Task BindingDataContext()
+    {
+        WeakReference reference;
+        var data = new { Text = "Foo" };
+
+        {
+            var binding = new Binding("Text") { Mode = BindingMode.OneWay };
+            var text = new TextBlock();
+            reference = new WeakReference(text);
+            text.DataContext = data;
             text.SetBinding(TextBlock.TextProperty, binding);
             Assert.Equal("Foo", text.Text);
         }
